ae63f169d3d107a7880531a47602153801b7485c
Stanley Karol <stanislove.korol@gmail.com>
2025-04-10 11:21:23 -0400

    complete ShoppingList app

---
+++ ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("1. Fresh Vegetables");
+        System.out.println("2. Fruits");
+        System.out.println("3. Protein Source");
+        System.out.println("4. Whole Grains");
+        System.out.println("5. Dairy or Alternatives");
+        System.out.println("6. Snacks");
+        System.out.println("7. Cooking Staples");
+        System.out.println("8. Cleaning Supplies");
+        System.out.println("9. Toiletries");
+        System.out.println("10. Emergency Quick Meals");
+    }
+}
